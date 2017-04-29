4b102a44b437b7f6b60860550b84a0d3b90486dd
Christian Martinez <cgmartinez11142@gmail.com>
2017-04-28 22:50:46 -0700

    add function to save question comments to database

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -1,14 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addingComment: false,
+
   actions: {
     saveComment() {
       var params = {
-        commentUser: this.get('user'),
-        commentBody: this.get('body'),
-        commentTimestamp: (new Date()).toJSON()
+        commentUser: this.get('commentUser'),
+        commentBody: this.get('commentBody'),
+        commentTimestamp: (new Date()).toJSON(),
+        question: this.get('question')
       };
+
       this.sendAction('saveComment', params)
+    },
+    showForm: function(){
+      this.set('addingComment', true);
     }
   }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    saveComment(params) {
+      var newComment = this.store.createRecord('comment', params);
+      var question = params.question;
+      question.get('comments').addObject(newComment);
+      newComment.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', question);
+    }
   }
 });
